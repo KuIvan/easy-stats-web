11ddd56285e266cacff1bb81c380a43e653b01a4
antklepikov <61699032+antklepikov@users.noreply.github.com>
2022-03-18 13:39:48 +0300

    Setup mui

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,11 @@
 import React, { useEffect, ReactElement, ReactNode } from 'react'
 import type { AppProps } from 'next/app'
 import type { NextPage } from 'next'
-
+import { ThemeProvider } from '@mui/material/styles'
 // src
 import '../styles/globals.css'
 import DefaultLayout from 'src/components/templates/DefaultLayout'
+import theme from "../src/styles/theme";
 
 type NextPageWithLayout = NextPage & {
   getLayout?: (page: ReactElement) => ReactNode
@@ -18,7 +19,11 @@ function MyApp({ Component, pageProps }: AppPropsWithLayout) {
 
   const getLayout = Component.getLayout ?? ((page) => <DefaultLayout>{page}</DefaultLayout>)
 
-  return getLayout(<Component {...pageProps} />)
+  return (
+    <ThemeProvider theme={theme}>
+      {getLayout(<Component {...pageProps} />)}
+    </ThemeProvider>
+  )
 }
 
 export default MyApp
