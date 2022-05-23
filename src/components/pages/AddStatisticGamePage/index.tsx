4c95aed55f24475914936e1625f141d48664e0d6
Vladimir <alik.mikhaylov.web@gmail.com>
2022-05-23 18:54:52 +0300

    add new Page(game-stats) and route for this page

---
+++ src/components/pages/AddStatisticGamePage/index.tsx
@@ -0,0 +1,168 @@
+import React, { useState } from 'react'
+import {
+  Box,
+  Button,
+  FormControl,
+  Grid,
+  InputLabel,
+  MenuItem,
+  Select,
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableRow,
+} from '@mui/material'
+
+//src
+import DefaultLandingTitle from '../../atoms/DefaultLandingTitle'
+import { axiosClient } from '../../../utils/apiUtils/api.config'
+
+interface AddStatisticPageProps {
+  gameId: number
+}
+
+export default function AddStatisticPage({ gameId }: AddStatisticPageProps) {
+
+  const [playerOne, setPlayerOne] = useState<number>(null);
+  const [playerSecond, setPlayerSecond] = useState<number>(null);
+  const [action, setAction] = useState<string>('');
+
+  const handleChange = (event: { target: { value: React.SetStateAction<string>; }; }, setValue: (event: any) => void) => {
+    setValue(event.target.value);
+  };
+
+  const formStatistic = [
+    {
+      id: 0,
+      label: 'Player #1',
+      menuItems: [{ id: 0, value: 17 }, { id: 1, value: 22 }, { id: 2, value: 23 }, { id: 3, value: 10 },],
+      value: playerOne,
+      setValue: setPlayerOne
+    },
+    {
+      id: 1,
+      label: 'Action',
+      menuItems: [{ id: 0, value: 'goal' }, { id: 1, value: 'assist' }, { id: 2, value: 'yellow card' }, {
+        id: 3,
+        value: 'red card'
+      },],
+      value: action,
+      setValue: setAction
+    },
+    {
+      id: 2,
+      label: 'Player #2',
+      menuItems: [{ id: 0, value: 17 }, { id: 1, value: 22 }, { id: 2, value: 23 }, { id: 3, value: 10 },],
+      value: playerSecond,
+      setValue: setPlayerSecond
+    },
+  ]
+
+  function newSelectInput() {
+    // axiosClient.post('/#', { playerOne, playerSecond, action })
+      // .then(
+    const newArray = [
+    ...rows,
+      {id: rows.size, initiator: playerOne, addressable: playerSecond, action: action }
+    ]
+        setAction('')
+        setPlayerOne(0)
+        setPlayerSecond(0)
+    return setRows(newArray)
+      // )
+  }
+
+  const [rows, setRows] = useState<any>( [
+    { id: 0, initiator: 17, addressable: 12, action: 'goal' },
+    { id: 1, initiator: 27, addressable: 22, action: 'assist' },
+    { id: 2, initiator: 37, addressable: 3, action: 'red card' },
+    { id: 3, initiator: 47, addressable: 42, action: 'yellow card' },
+
+  ])
+
+  function deleteRecord(id: number) {
+    const newArray = [
+      ...rows.slice(0, id),
+      ...rows.slice(id + 1)
+    ]
+    return   setRows(newArray)
+  }
+
+  return (
+    <Grid
+      container
+      spacing={5}
+      justifyContent='center'
+      sx={{ marginTop: 10 }}
+    >
+      <Grid item xs={12}>
+        <DefaultLandingTitle title={'Add statistic'}/>
+      </Grid>
+      <Grid item xs={12}>
+
+        <Grid
+          container
+          spacing={5}
+          justifyContent='center'
+        >
+          {formStatistic.map((form) => (
+            <Grid item xs={2}>
+              <Box sx={{ minWidth: 120 }} key={form.id}>
+                <FormControl fullWidth>
+                  <InputLabel>{form.label}</InputLabel>
+                  <Select
+                    value={form.value}
+                    label={form.label}
+                    onChange={(event) => handleChange(event, form.setValue)}
+                  >
+                    {form.menuItems.map((item) => (
+                      <MenuItem key={item.id} value={item.value}>{item.value}</MenuItem>
+                    ))}
+                  </Select>
+                </FormControl>
+                <Grid item xs={1}>
+                </Grid>
+              </Box>
+            </Grid>
+          ))}
+          <Grid item xs={1}>
+            <Button
+              onClick={newSelectInput}
+              disabled={(!action || !playerOne) || (playerOne == playerSecond)}
+            >
+              Add
+            </Button>
+          </Grid>
+        </Grid>
+      </Grid>
+      <Grid item xs={6}>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell align="left">№</TableCell>
+                <TableCell align="left">Player One</TableCell>
+                <TableCell align="left">Action</TableCell>
+                <TableCell align="left">Player Second</TableCell>
+                <TableCell align="left">Delete</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {rows.map((row: any, index: number) => (
+                <TableRow key={row.id}>
+                  <TableCell>{index+1}</TableCell>
+                  <TableCell align="left">{row.initiator}</TableCell>
+                  <TableCell align="left">{row.action}</TableCell>
+                  <TableCell align="left">{row.addressable}</TableCell>
+                  <TableCell align="left">
+                    <Button onClick={() => {deleteRecord(row.id)}}>Delete</Button>
+                  </TableCell>
+
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+      </Grid>
+    </Grid>
+  )
+}
